004f1b0deaba4761e30a077add66a8a2a999b91d
Predmet <175954199+impredmet@users.noreply.github.com>
2024-11-07 13:29:29 -0500

    fix: refactor constants in index.ts and add DEFAULT_HD_PATH

--- src/shared/constant/index.ts
+++ src/shared/constant/index.ts
@@ -7,9 +7,7 @@ export const KEYRING_TYPE = {
 };
 
 export const IS_CHROME = /Chrome\//i.test(navigator.userAgent);
-
 export const IS_LINUX = /linux/i.test(navigator.userAgent);
-
 export const IS_WINDOWS = /windows/i.test(navigator.userAgent);
 
 export const ADDRESS_TYPES: {
@@ -25,6 +23,7 @@ export const ADDRESS_TYPES: {
     hdPath: "m/44'/0'/0'/0",
   },
 ];
+export const DEFAULT_HD_PATH = ADDRESS_TYPES[0].hdPath;
 
 export const EVENTS = {
   broadcastToUI: "broadcastToUI",
@@ -37,22 +36,20 @@ export const EVENTS = {
   },
 };
 
-const CONTENT_URL =
-  process.env.CONTENT_URL ?? "https://content.nintondo.io/api/pub";
-const HISTORY_URL =
-  process.env.HISTORY_URL ?? "https://history.nintondo.io/pub";
-
+export const LUCKYCOIN_URL = "https://luckycoinfoundation.org";
 export const API_URL = "https://luckycoinexplorer.com";
 export const EXPLORER_URL = "http://luckycoin-explorer.com";
-
 export const RPC_URL = "https://feelinglucky.satsignal.io";
 
-export const LUCKYCOIN_URL = "https://luckycoinfoundation.org";
-export const SPLITTER_URL = LUCKYCOIN_URL + "/belinals/splitter";
-
+const CONTENT_URL =
+  process.env.CONTENT_URL ?? "https://content.nintondo.io/api/pub";
 export const getContentUrl = () => CONTENT_URL;
 
+const HISTORY_URL =
+  process.env.HISTORY_URL ?? "https://history.nintondo.io/pub";
 export const getHistoryUrl = () => HISTORY_URL;
+
+export const SPLITTER_URL = LUCKYCOIN_URL + "/belinals/splitter";
 
 export const DEFAULT_FEES = {
   fast: 20000,
@@ -60,5 +57,3 @@ export const DEFAULT_FEES = {
 };
 
 export const DEFAULT_SERVICE_FEE = 1_000_000;
-
-export const DEFAULT_HD_PATH = ADDRESS_TYPES[0].hdPath;
